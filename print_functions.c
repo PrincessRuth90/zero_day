3e1650c1cc8c1469926585faa21fecfec3647057
PrincessRuth90 <marcusruth86@gmail.com>
2022-07-11 03:46:16 -0700

    Print integers

---
+++ print_functions.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_int - prints an integer.
+ * @arg: argument
+ * Return: Always 0
+ */
+
+int print_int(va_list arg)
+{
+
+unsigned int divisor = 1, i, resp, charPrinted = 0;
+int n = va_arg(arg, int);
+
+if (n < 0)
+{
+_putchar('-');
+charPrinted++;
+n *= -1;
+}
+
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+
+
+for (; divisor >= 1; n %= divisor, divisor /= 10, charPrinted++)
+{
+resp = n / divisor;
+_putchar('0' + resp);
+}
+return (charPrinted);
+}
